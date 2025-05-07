ee350623d8c7766689d372c71703b002b19792e8
Suchit Balaso Kharade <suchitkharade924@gmail.com>
2025-05-07 13:59:41 +0530

    updates

--- config.py
+++ config.py
@@ -1,7 +1,11 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    STRIPE_PUBLIC_KEY = 'ypk_test_51RJCnKPLKdDMzpK8pIs4mfp5oP187jgywxUVnOrr5TIyJKL1olHKCYgn0tUu3HSuIHIMdhzNM6Lhzim385nNhaBy00SPwxya85'
-    STRIPE_SECRET_KEY = 'sk_test_51RJCnKPLKdDMzpK89oxGEccXI9OVJ4VO2LOu4KEOjh64Ftzj78ScAA7my9b4otiSHjjEu1XASzSMyHHA9FVLStp900CLqPaDuc'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+    STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
